b2f157c9a0fbbc4b85a42b09e046c5849f3c891d
FlyFish0825 <820145912@qq.com>
2025-09-02 17:36:32 +0800

    modified:   main.c

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-
+int add(int a,int b)
+{
+    return a + b;
+}
 int main()
 {
     printf("Hello, World!\n");
+    printf("The sum of 5 and 10 is: %d\n", add(5, 10));
     return 0;
 }
