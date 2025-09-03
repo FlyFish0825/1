99c56c3bdad41263def87eadf9af0ae7e194179f
FlyFish0825 <820145912@qq.com>
2025-09-03 11:34:48 +0800

    modified:   .gitignore 	new file:   CmakeLists.txt 	new file:   inc/hello.h 	modified:   main.c 	new file:   src/hello.c

--- main.c
+++ main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
+#include "hello.h"
 
-int add(int a,int b)
-{
-    return a + b;
-}
 int main()
 {
-    printf("Hello, World!\n");
-    printf("The sum of 5 and 10 is: %d\n", add(5, 10));
+    hello();
     return 0;
 }
