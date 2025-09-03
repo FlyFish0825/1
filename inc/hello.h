854cebc9d9ac35e3eae84f31ee293dd15a40bea3
Du_shiyu <820145912@qq.com>
2025-09-03 22:25:34 +0800

    modified:   .clangd 	modified:   .gitignore 	new file:   build/CMakeFiles/4.1.0/CompilerIdC/CMakeCCompilerId.c 	new file:   inc/Mymath.h 	modified:   inc/hello.h 	new file:   src/Mymath.c 	modified:   src/hello.c

--- inc/hello.h
+++ inc/hello.h
@@ -1,6 +1,7 @@
 #ifndef __HELLO_H__
 #define __HELLO_H__
-
+#include "Mymath.h"
+#include "stdio.h"
 void hello(void);
-
+int hello_add(int a,int b);
 #endif
---
+++ src/Mymath.c
@@ -0,0 +1,5 @@
+#include "Mymath.h"
+int add(int a,int b)
+{
+    return a+b;
+}
