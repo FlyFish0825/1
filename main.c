b52f83207019db8cb026901ac336d305c998cf58
FlyFish0825 <820145912@qq.com>
2025-09-03 14:57:33 +0800

    modified:   main.c

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "hello.h"
 
+/**
+ * 主函数程序入口
+ * @return 返回整型值，表示程序执行状态
+ */
 int main()
-{
-    hello();
-    return 0;
+{  // 主函数，程序的入口点
+    hello();  // 调用hello函数
+    return 0;  // 返回0表示程序正常结束
 }
